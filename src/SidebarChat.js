a6206bc0070556d79f1cf802195af31b8e5146ad
Lokindra Dangi <lokendradangi0@gmail.com>
2020-10-11 12:35:29 +0545

    Romms are fetch from database

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import { Avatar } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import "./SidebarChat.css";
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ addNewChat, id, name }) {
   const [seed, setSeed] = useState("");
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -18,7 +18,7 @@ function SidebarChat({ addNewChat }) {
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
-        <h2>Room Name</h2>
+        <h2>{name}</h2>
         <p>Last Message</p>
       </div>
     </div>
