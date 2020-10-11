abde55a36216cfdb07d6e409e0c27e7e3884af77
Lokindra Dangi <lokendradangi0@gmail.com>
2020-10-11 15:07:45 +0545

    Signin with Google is Complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Chat from "./Chat";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Login";
+import { useStatevalue } from "./StateProvider";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [{ user }, dispatch] = useStatevalue();
 
   return (
     <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Login.css";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
+import { useStatevalue } from "./StateProvider";
+import { actionTypes } from "./reducer";
 
 function Login() {
+  const [{ user }, dispatch] = useStatevalue();
+
   const signIn = () => {
-    console.log("signin");
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
   return (
     <div className="login">
